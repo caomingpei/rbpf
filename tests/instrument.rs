61b91d8c2be0963411d7a183e5004f9ff538473f
caomingpei <caomingpei@outlook.com>
2024-11-29 12:51:00 +0000

    !T: add parser_duplicate_input

--- tests/instrument.rs
+++ tests/instrument.rs
@@ -3,7 +3,7 @@ use solana_rbpf::instrument::*;
 
 /// Generate a account
 /// Parameters:
-/// - duplicate_index: the index of the duplicate account, if no duplicate, set to 0
+/// - duplicate_index: the index of the duplicate account, if no duplicate, input 0xff
 /// Return:
 /// - a account (Vec<u8>), MUST be 0x2860 bytes or 0x08 bytes
 fn case_generate_account(duplicate_index: u8) -> Vec<u8> {
@@ -14,7 +14,7 @@ fn case_generate_account(duplicate_index: u8) -> Vec<u8> {
     // number of bytes of account data: 4
     // account data: 0x05..(4)
     // rent epoch: 0x06..(8)
-    if duplicate_index > 0 {
+    if duplicate_index != 0xff {
         // 0x01: index of the duplicate account
         return vec![duplicate_index, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00];
     }
@@ -58,6 +58,13 @@ fn case_generate_account(duplicate_index: u8) -> Vec<u8> {
     account
 }
 
+
+/// Generate a instruction
+/// Parameters:
+/// - number: the number of instructions
+/// Return:
+/// - a instruction (Vec<u8>)
+/// Note: the return instruction include 8 bytes of the instruction number
 fn case_generate_instruction(number: u64) -> Vec<u8> {
     let mut instruction: Vec<u8> = vec![];
     const BYTE_MAX: u64 = u8::MAX as u64;
@@ -69,39 +76,85 @@ fn case_generate_instruction(number: u64) -> Vec<u8> {
     instruction
 }
 
-fn case_single_input(instruction_number: u64) -> Vec<u8> {
-    // one account, not duplicate, is signer, is writable, not executable
-    // instruction: 0x0102030405060708090a
-    // program id: 0xaa..(32)
+
+/// Generate a input with not duplicate accounts
+/// Parameters:
+/// - account_number: the number of accounts
+/// - instruction_number: the number of instructions
+/// Return:
+/// - a input (Vec<u8>)
+fn case_not_duplicate_input(account_number: u64, instruction_number: u64) -> Vec<u8> {
     let mut input: Vec<u8> = vec![];
-    input.push(0x01);
-    for _ in 0..7 {
-        input.push(0x00);
-    } // account number
-    // account not duplicate, is signer, is writable, is executable
-    let account = case_generate_account(0);
-    input.extend_from_slice(&account);
-    let instruction = case_generate_instruction(instruction_number);
-    input.extend_from_slice(&instruction);
+    input.extend_from_slice(&account_number.to_le_bytes());
+    for _ in 0..account_number {
+        input.extend_from_slice(&case_generate_account(0xff));
+    }
+    input.extend_from_slice(&case_generate_instruction(instruction_number));
     for _ in 0..32 {
         input.push(0xaa);
     } // program id
-
     input
 }
 
-fn case_multiple_not_duplicate_input(account_number: u64, instruction_number: u64) -> Vec<u8> {
+/// Generate a input with multiple duplicate accounts
+/// Parameters:
+/// - account_number: the number of accounts
+/// - instruction_number: the number of instructions
+/// - duplicates: the indices of the duplicate accounts, 0xff means not duplicate, others means the index of the duplicate account
+/// Return:
+/// - a input (Vec<u8>)
+fn case_duplicate_input(account_number: u64, instruction_number: u64, duplicates: &[u8]) -> Vec<u8> {
+    if account_number < 2 {
+        panic!("account_number must be greater than 1");
+    }
     let mut input: Vec<u8> = vec![];
+    input.extend_from_slice(&account_number.to_le_bytes());
+    for &flag in duplicates {
+        let account = case_generate_account(flag);
+        input.extend_from_slice(&account);
+    }
+    input.extend_from_slice(&case_generate_instruction(instruction_number));
+    for _ in 0..32 {
+        input.push(0xaa);
+    }
     input
 }
-
 
 #[test]
-fn parser_hello_world() {
-    let instruction_number: u64 = 10;
-    let input = case_single_input(instruction_number);
+fn parser_not_duplicate_input() {
+    let mut instruction_number: u64 = 10;
+    let mut input = case_not_duplicate_input(1, instruction_number);
     assert_eq!(
         input.len() as u64,
         8 + parser::ACCOUNT_SIZE + 8 + instruction_number + 32
     ); // 8: account number, ACCOUNT_SIZE: account size (duplicate flag + 0x285f), 8: instruction number, 32: program id
+
+    instruction_number = 100;
+    input = case_not_duplicate_input(2, instruction_number);
+    assert_eq!(
+        input.len() as u64,
+        8 + parser::ACCOUNT_SIZE * 2 + 8 + instruction_number + 32
+    );
+}
+
+#[test]
+fn parser_duplicate_input() {
+    let instruction_number: u64 = 10;
+    let duplicates: Vec<u8> = vec![0xff, 0xff, 0xff, 0x01];
+    let account_number: u64 = duplicates.len() as u64;
+    let input = case_duplicate_input(account_number, instruction_number, &duplicates);
+
+    let mut account_all_size: u64 = 0;
+    for &flag in &duplicates {
+        if flag == 0xff {
+            account_all_size += parser::ACCOUNT_SIZE;
+        } else {
+            account_all_size += 8;
+        }
+    }
+    
+    assert_eq!(
+        input.len() as u64,
+        8 + account_all_size + 8 + instruction_number + 32
+    );
 }
